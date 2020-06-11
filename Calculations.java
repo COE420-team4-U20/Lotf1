3820c9e42f516d9058cf893b60a23d43353f7a74
Ahmed <victorylover987@gmail.com>
2020-06-11 15:06:06 +0400

    initial commit

---
+++ Calculations.java
@@ -0,0 +1,20 @@
+public class Calculations {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 1;
+		
+		//sum of x and y 
+		int sum = x+y;
+		System.out.println("The sum is= "+ sum);
+		
+		//member_B add the feature of subtracting x from y and print the result
+		
+		//member_C add the feature of dividing x by y and print the result
+		
+		//member_D add the feature of multiplying x by y and print the result
+		
+		
+	}
+
+}
